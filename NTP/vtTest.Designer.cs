30f3734dc0bc558e64ed183a4909cbef3f0667b2
ekrem <ekrem@DESKTOP-F4RSLUB>
2022-04-01 12:13:37 +0300

    güncelleme

--- NTP/vtTest.Designer.cs
+++ NTP/vtTest.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(514, 453);
+            this.ClientSize = new System.Drawing.Size(720, 565);
             this.Controls.Add(this.button1);
             this.Name = "vtTest";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
